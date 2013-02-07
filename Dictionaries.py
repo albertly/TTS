18dda98449985a665e1e51c3bcc944e49793e13e
albertly <albertly@yandex.ru>
2013-02-07 21:50:58 +0200

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,11 +1,11 @@
-import urllib
+iimport urllib
 import re
 from HTMLParser import HTMLParser
 from htmlentitydefs import name2codepoint
-
+import xml.etree.ElementTree as ET
 
 class str_cir(str):
-  ''' A string with a built-in case-insensitive replacement method '''
+	''' A string with a built-in case-insensitive replacement method '''
 	
 	def ireplace(self,old,new,count=0):
 		''' Behaves like S.replace(), but does so in a case-insensitive
@@ -88,6 +88,54 @@ class YourDictionaryParser(HTMLParser):
             self.entity = Entity()
             self.entity.setMeaning(data)
         
+class MerriamWebsterParser() :
+	def __init__(self, word):
+		self.data = []
+		self.word = word
+		self.words = []
+#http://www.dictionaryapi.com/api/v1/references/learners/xml/root?key=91359489-f427-4143-9465-5b3afadd3d27
+		xml = urllib.urlopen('http://www.dictionaryapi.com/api/v1/references/collegiate/xml/' + word + "?key=181c71fa-4b20-4ec3-83d8-5eb06fe8bdf0").read()
+		root = ET.fromstring(xml)
+		for child in root:
+			if child.find('./ew').text == word :
+				childrens = child.findall('*/dt')
+				self.words = self.words + childrens
+				print "hild     :", child.text, child.tag, child.attrib, child.find('./ew').text
+				for w in childrens :
+					entity = Entity()
+					meaning = w.text
+					if type(w.tail) == str : meaning = meaning + " " + w.tail
+					entity.meaning = meaning
+					print "Meaning  :", w.tag, w.text
+					for c in w :
+						if c.tag == 'vi' :
+							example = c.text
+							if type(c.tail) == str : example = example + " " + c.tail
+							entity.setExample(example)
+							print "ex     :", c.text    
+						elif c.tag == 'fw' :
+							meaning = c.text
+							if type(c.tail) == str : meaning = meaning + " " + c.tail
+							entity.meaning = entity.meaning + " " + meaning
+							print "MeaningA  :", c.tag, c.text, c.tail
+						else :
+							print "MeaningR  :", c.tag, c.text, c.tail
+					self.data.append(entity)
+					
+	def format(self) :
+		st = "<ul>"
+		for e in self.data :
+			st = st + "<li>" +  str_cir(e.meaning).ireplace(self.word, " ___ ") + "</li>"
+			st = st + "<ul>"
+			for ex in e.examples :
+				st = st + "<li>" + str_cir(ex).ireplace(self.word, " ___ ") + "</li>"
+			st = st + "</ul>" 
+		st = st + "</ul>"        
 
-
+		return st
+	def format1(self) :
+		for w in self.words :
+			print "Meaning    :", w.text, w.tail, w.tag
+			for c in w :
+			      print "Meanin1    :", c.text, c.tail, c.tag 
 
